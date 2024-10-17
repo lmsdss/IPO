a76ec2facd169d88cc9991ed011e24060bd41cb1
wenfangsun <87852654+lmsdss@users.noreply.github.com>
2024-10-17 23:40:56 +0800

    Add files via upload

---
+++ dassl/evaluation/build.py
@@ -0,0 +1,12 @@
+from dassl.utils import Registry, check_availability
+
+EVALUATOR_REGISTRY = Registry("EVALUATOR")
+
+
+def build_evaluator(cfg, **kwargs):
+    avai_evaluators = EVALUATOR_REGISTRY.registered_names()
+    check_availability(cfg.TEST.EVALUATOR, avai_evaluators)
+    if cfg.VERBOSE:
+        pass
+        # print("Loading evaluator: {}".format(cfg.TEST.EVALUATOR))
+    return EVALUATOR_REGISTRY.get(cfg.TEST.EVALUATOR)(cfg, **kwargs)
